9a5f86f6aaec2cc7ae7f693038b9896054457e25
Sandro <sandro.delrivo@gmail.com>
2017-07-07 15:08:43 -0700

    Added potentially working drawer

--- js/Index.js
+++ js/Index.js
@@ -4,9 +4,13 @@ import HTMLView from 'react-native-htmlview';
 
 import { AppStyles, AppTextStyles } from './Styles';
 import { BoxAlert, Announcement } from './UI';
+import Drawer from 'react-native-drawer-menu';
+import {Easing} from 'react-native';
 
 
 export class IndexScreen extends Component {
+
+
   static navigationOptions = {
     title: 'Spartan Connect',
     headerRight: <Text>Weather: Impossible 72&deg;  </Text>
@@ -68,14 +72,54 @@ export class IndexScreen extends Component {
   }
 
   render() {
+
+    // Drawer Setup
+    var drawerContent = (<View style={styles.drawerContent}>
+    <View style={styles.leftTop}/>
+    <View style={styles.leftBottom}>
+      <View><Text>Drawer Content</Text></View>
+    </View>
+    </View>);
+    // customize drawer's style (Optional)
+    var customStyles = {
+      drawer: {
+        shadowColor: '#000',
+        shadowOpacity: 0.4,
+        shadowRadius: 10
+      },
+    mask: {}, // style of mask if it is enabled
+    main: {} // style of main board
+    };
+
     const { navigate } = this.props.navigation;
     return (
-      <ScrollView refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this._onRefresh.bind(this)}/>} style={AppStyles.announcementsView}>
-        <StatusBar hidden />
-        <Button onPress={() => navigate('Announcement', {id: 1})} title="Open Sample Announcement" />
-        <BoxAlert title="DEVELOPER'S NOTICE" description="This is not meant to be used as the final app or even the development app during bootcamp. This simply serves as a prototype for developers to learn from."/>
-        <FlatList data={this.state.announcements} renderItem={({item}) => <Announcement id={item.value} returnFunction={this._onRedirect.bind(this)} />}/>
-      </ScrollView>
+      <View>
+        <ScrollView refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this._onRefresh.bind(this)}/>} style={AppStyles.announcementsView}>
+          <StatusBar hidden />
+          <Button onPress={() => navigate('Announcement', {id: 1})} title="Open Sample Announcement" />
+          <BoxAlert title="DEVELOPER'S NOTICE" description="This is not meant to be used as the final app or even the development app during bootcamp. This simply serves as a prototype for developers to learn from."/>
+          <FlatList data={this.state.announcements} renderItem={({item}) => <Announcement id={item.value} returnFunction={this._onRedirect.bind(this)} />}/>
+        </ScrollView>
+
+        // Drawer Implementation
+        <Drawer
+          style={styles.container}
+          drawerWidth={300}
+          drawerContent={drawerContent}
+          type={Drawer.types.Overlay}
+          customStyles={{drawer: styles.drawer}}
+          drawerPosition={Drawer.positions.Right}
+          onDrawerOpen={() => {console.log('Drawer is opened');}}
+          onDrawerClose={() => {console.log('Drawer is closed')}}
+          easingFunc={Easing.ease}
+         >
+          <View style={styles.content}>
+            <Text>{Object.values(Drawer.positions).join(' ')}</Text>
+            <Text>{Object.values(Drawer.types).join(' ')}</Text>
+          </View>
+        </Drawer>
+      </View>
+
     );
   }
 }
